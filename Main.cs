e58f5086ef0f58a60702a4dc73baec605c549209
ElJorko <jjorqueragrau@gmail.com>
2015-05-22 00:39:54 +0200

    Initial commit
    Yasta initial commit

---
+++ Main.cs
@@ -0,0 +1,16 @@
+using System;
+using Gtk;
+
+namespace Yasta
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
